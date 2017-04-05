4a7010854fe5f3eead2d4ec6c943a7a313d42d94
Matt Tuttle <mattjtuttle@gmail.com>
2017-04-05 14:26:33 -0400

    Documented task09 function

--- R/add2.R
+++ R/add2.R
@@ -1,3 +1,14 @@
+#' add2
+#'
+#' Adds two numbers together.
+#'
+#' @param a A number.
+#' @param b A number.
+#' @return The sum of \code{a} and \code{b}
+#' @examples
+#' add2(1, 2)
+#' @export
+
 add2 <- function(a, b) {
   return(a + b)
 }
