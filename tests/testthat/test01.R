f0c383fcf4eabe81547983814fba971ca1b5d4f6
Matt Tuttle <mattjtuttle@gmail.com>
2017-04-05 16:43:29 -0400

    Updated unit test

--- tests/testthat/test01.R
+++ tests/testthat/test01.R
@@ -1,6 +1,4 @@
-library(testthat)
-
 #Tests to make sure that 1 and 1 makes 2
-test_that("Numbers are being added properly", {
-  expect_equal(add2(1, 1), sum(1, 1))
+testthat::test_that("Numbers are being added properly", {
+  testthat::expect_equal(add2(1, 1), sum(1, 1))
 })
